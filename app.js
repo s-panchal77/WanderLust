dab1453083d6da53152ade518b3edb340ce26dc0
Sarthak Panchal <sarthak7704@gmail.com>
2026-03-26 16:55:58 +0530

    Show Route

--- app.js
+++ app.js
@@ -22,16 +22,24 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hi, i am root");
 });
 
+// Index Route
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index", { allListings });
 });
 
+// Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show", { listing });
+});
 
 // app.get("/testListing", async (req,res) =>{
 //   let sampleListing = new Listing({
